91a340655a911f825d4ed0bbe0b8583820e189bb
Alex <alexbkennedy96@gmail.com>
2021-06-02 20:06:05 -0400

    Uses the useEffect() hook for error validation on inputs using state

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Card from "../UI/Patterns/Card/Card";
-import classes from "./Login.module.scss";
-import uiHelpers from "../UI/Helpers/Helpers.module.scss";
 import Button from "../UI/Patterns/Button/Button";
 import TextInput from "../UI/Patterns/TextInput/TextInput";
+
+import classes from "./Login.module.scss";
+import uiHelpers from "../UI/Helpers/Helpers.module.scss";
 
 const Login = (props) => {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -13,20 +14,18 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    setFormIsValid(
+      enteredEmail.includes("@") && enteredPassword.trim().length > 6
+    );
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (value) => {
     setEnteredEmail(value);
-
-    setFormIsValid(
-      value.includes("@") && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (value) => {
     setEnteredPassword(value);
-
-    setFormIsValid(
-      value.trim().length > 6 && enteredEmail.includes("@")
-    );
   };
 
   const validateEmailHandler = () => {
@@ -49,6 +48,7 @@ const Login = (props) => {
           label="Email"
           type="email"
           id="email"
+          className={emailIsValid === false && 'invalid'}
           value={enteredEmail}
           onChangeHandler={emailChangeHandler}
           onBlurHandler={validateEmailHandler}
@@ -57,6 +57,7 @@ const Login = (props) => {
           label="Password"
           type="password"
           id="password"
+          className={passwordIsValid === false && 'invalid'}
           value={enteredPassword}
           onChangeHandler={passwordChangeHandler}
           onBlurHandler={validatePasswordHandler}
